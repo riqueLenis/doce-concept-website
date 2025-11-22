0b37150cb3224170e1d4ac596576b7fd43771ef4
Henrique Lenis Dev <henriquelenis12@gmail.com>
2025-11-22 12:18:37 -0300

    adjusted layout icons and na links

--- js/nav.js
+++ js/nav.js
@@ -1,28 +1,22 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li')
-    
-    burger.addEventListener('click',()=> {
-        //toggle nav
-        nav.classList.toggle('nav-active');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-          // animate links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation){
-                link.style.animation = ''
-            }
-            else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.6 }s`;
-            }  
-        });
-        //burger animation
-        burger.classList.toggle('toggle');
-  
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.6
+        }s`;
+      }
     });
-
- 
-}
-
+    burger.classList.toggle("toggle");
+  });
+};
 
 navSlide();
